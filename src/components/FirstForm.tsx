6f35866c5a7b0cf389cc4075ef34d1c98869f9fd
AliAsgharGill <stylinalivlogs@gmail.com>
2024-07-05 20:23:05 +0500

    Custom Validation & Fundamental Completed

--- src/components/FirstForm.tsx
+++ src/components/FirstForm.tsx
@@ -26,6 +26,7 @@ const FirstForm = () => {
         <h1 className="text-black font-bold ">Form ({renderCount / 2})</h1>
         <form
           noValidate
+        //   to submit the form we use handleSubmit method assigned to the onSubmit event and passed in our submit function
           onSubmit={handleSubmit(onSubmit)}
           className=" bg-white shadow-md p-4 rounded "
         >
@@ -35,6 +36,10 @@ const FirstForm = () => {
               type="text"
               id="name"
               className="border border-black rounded"
+            // to register a field with RHF need to use ...register() method, it automatically track the field state
+
+            // and for validation we can pass options to the register function
+
               {...register("username", {
                 required: {
                   value: true,
@@ -57,8 +62,27 @@ const FirstForm = () => {
                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                   message: "Invalid email format",
                 },
+                // we can do custom validation using the validate function
+                validate: {
+                  required: (fieldValue) => {
+                    return fieldValue.trim() !== "" || "Email Address Required";
+                  },
+                  notAdmin: (fieldValue) => {
+                    return (
+                      fieldValue !== "admin@gmail.com" ||
+                      "Enter a different email address"
+                    );
+                  },
+                  notBlackListed: (fieldValue) => {
+                    return (
+                      !fieldValue.endsWith("baddomain.com") ||
+                      "This domain is not supported"
+                    );
+                  },
+                },
               })}
             />
+            {/* In any error case of current field error message will shown becuase we get errors from formState */}
             <p className="my-1 text-sm text-red-500">{errors.email?.message}</p>
           </div>
           <div className="flex justify-start  text-start flex-col">
